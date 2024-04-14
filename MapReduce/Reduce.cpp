ca0b01a53ac6b463d864f8bd5a4149a3e421be4e
cklnunokawa <64506408+cklnunokawa@users.noreply.github.com>
2024-04-14 13:19:10 -1000

    Update Reduce.cpp

--- MapReduce/Reduce.cpp
+++ MapReduce/Reduce.cpp
@@ -1,1 +1,15 @@
 #include "Reduce.h"
+
+void Reduce::exportReduce(string key, int reducedValue) {
+    
+	if (std::filesystem::exists(".\\" + "theOutput.txt"))
+	{}
+	else {
+		int createOutput = FileManagement::createFile(".\\" + "theOutput.txt");
+	}
+    int added=FileManagement::writeDataToFile(".\\" + "theOutput.txt", key, reducedValue);
+	if (added==0) {
+		int createSuccess = FileManagement::createFile(".\\" + "SUCCESS.txt");
+	}
+
+}
