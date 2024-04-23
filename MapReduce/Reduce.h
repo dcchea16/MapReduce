299d37c4e17b9f886fdd1cf3aa8c8856977b168c
cklnunokawa <64506408+cklnunokawa@users.noreply.github.com>
2024-04-23 12:02:36 -1000

    adding comments

--- MapReduce/Reduce.h
+++ MapReduce/Reduce.h
@@ -2,18 +2,22 @@
 
 #include <iostream>
 #include <vector>
-
 using std::string;
 using std::vector;
 
+//Reduce class that takes in a string key and a vector of numbers and adds the numbers in the vector together to a single interger and outputs it to a file
 class Reduce
 {
 public:
+	//constructor that takes in the directory that the export file should be in and saves it as a class parameter, theDir
 	Reduce(string theDirInput)
 		:theDir(theDirInput) {};
+	//function that takes in the string key and vector of intergers, sums the vector, and passes the sum and key to the export function to output it to a file
 	int reduce(string keyInput, vector<int> countInput);
 
 private:
+	//export function called by reduce function that accepts the key and vector sum, it creates a file in the theDir directory if it does not exist and adds the key and interger to the file
 	int exportReduce(string key, int reducedValue);
+	//variable to hold the directory that is used in the exportReduce function
 	string theDir;
 };
