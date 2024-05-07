2ccaca22a0c11a2232810aa48ad032de69ad6358
cklnunokawa <64506408+cklnunokawa@users.noreply.github.com>
2024-05-06 18:59:32 -1000

    Update Map.h

--- MapReduce/Map.h
+++ MapReduce/Map.h
@@ -1,6 +1,50 @@
 #pragma once
-class Map
-{
+
+#ifdef MAPLIBRARY_EXPORTS
+#define MAPLIBRARY_API __declspec(dllexport)
+#else
+#define MAPLIBRARY_API __declspec(dllimport)
+#endif
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#define DllExport   __declspec( dllexport )
+using std::string;
+using std::unordered_map;
+
+//abstract parent class for Map class
+class PMap {
+public:
+    virtual void map(const string& key, const string& value) {};
+
 };
 
-/* Map.h */
+
+
+// Actual Map class is only used internally and is not exposed
+class Map:public PMap 
+{
+public:
+    // Constructor to initialize the Map with a directory for temporary storage
+    Map(string tempDir) : tempDirectory(tempDir) {}  // Initializes the tempDirectory member with the provided directory path
+
+    // Process a key-value pair by tokenizing and counting words
+    void map(const string& key, const string& value);
+
+    // Export the current word counts to a file
+    void exportToFile();
+
+private:
+    unordered_map<string, int> wordCount; // Hash table to store word counts
+    void flushBuffer(); // Flush remaining words from buffer to file
+    int bufferCount = 0; // Buffer Tracker
+    const int bufferThreshold = 100; // Threshold for the buffer
+    string fileName; // Name of the temp file where word counts are stored
+
+    const string tempDirectory; // Directory path for storing temp files
+};
+
+// Factory function to create a Map class and return a pointer
+extern "C" MAPLIBRARY_API PMap * createMap(string inputTemp);
+
