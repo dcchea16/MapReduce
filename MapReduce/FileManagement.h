45dd19d1dbdf32c28983aa3f3034aefbddf35203
cklnunokawa <64506408+cklnunokawa@users.noreply.github.com>
2024-05-06 20:19:02 -1000

    Update FileManagement.h

--- MapReduce/FileManagement.h
+++ MapReduce/FileManagement.h
@@ -1,29 +1,32 @@
-/* 
-This is the header file for the FileManagement class. This will contain all of the function declarations
-for the program and its private members.
-*/
-
 #pragma once
 #include <iostream>
-
 using std::string;
+// FileManagement.h 
+#pragma once
+#ifdef FILEMANAGEMENTLIBRARY_EXPORTS
+#define FILEMANAGEMENTLIBRARY_API __declspec(dllexport)
+#else
+#define FILEMANAGEMENTLIBRARY_API __declspec(dllimport)
+#endif
 
-class FileManagement
-{
-public:
 
-	// Default constructor
-	FileManagement() = default;
+// Function that will create a directory, given a directory path as dirPath
+extern "C" FILEMANAGEMENTLIBRARY_API int createDirectory(const string& dirPath);
+// Function that will check if a directory exists, given a directory path as dirPath
+extern "C" FILEMANAGEMENTLIBRARY_API int isDirectoryPresent(const string& dirPath);
+// Function that will check if a directory is empty, given a directory path as dirPath
+extern "C" FILEMANAGEMENTLIBRARY_API int isDirectoryEmpty(const string& dirPath);
 
-	// Destructor
-	~FileManagement() = default;
-	
-	static string readDatafromFile(const string& filePath);
-	static int writeDataToFile(const string& filePath, const string& data);
+// Function that will read all file contents from a directory, given a directory path as dirPath
+extern "C" FILEMANAGEMENTLIBRARY_API int readAllDirectoryFileContents(const string& dirPath);
+// Function that will delete all file contents from a directory, given a directory path as dirPath
+extern "C" FILEMANAGEMENTLIBRARY_API int deleteDirectoryContents(const string& dirPath);
 
-	static int createDirectory(const string& dirPath);
-	static int deleteDirectory(const string& dirPath);
-
-	static int createFile(const string& filePath);
-	static int deleteFile(const string& filePath);
-};
+// Function that will create a single file, given a file path as filePath
+extern "C" FILEMANAGEMENTLIBRARY_API int createFile(const string& filePath);
+// Function that will delete a single file, given a file path as filePath
+extern "C" FILEMANAGEMENTLIBRARY_API int deleteFile(const string& filePath);
+// Function that will read and output the data from a file as a string
+extern "C" FILEMANAGEMENTLIBRARY_API string readDatafromFile(const string& filePath);
+// Function that will write to a file, given a file path as filePath and input as data
+extern "C" FILEMANAGEMENTLIBRARY_API int writeDataToFile(const string& filePath, const string& data);
