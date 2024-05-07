4056b2f7a7a0dca2f6341fde155a6f38e9928041
cklnunokawa <64506408+cklnunokawa@users.noreply.github.com>
2024-05-06 18:24:11 -1000

    Update Reduce.h

--- MapReduce/Reduce.h
+++ MapReduce/Reduce.h
@@ -1,18 +1,44 @@
 #pragma once
+#ifdef REDUCELIBRARY_EXPORTS
+#define REDUCELIBRARY_API __declspec(dllexport)
+#else
+#define REDUCELIBRARY_API __declspec(dllimport)
+#endif
 
 #include <iostream>
 #include <vector>
-using std::vector;
+#define DllExport   __declspec( dllexport )
 using std::string;
-class Reduce
+using std::vector;
+
+//abstract parent class for the reduce class
+class PReduce {
+public:
+	//virtual reduce function
+	virtual int reduce(string key, vector<int> count) { return 1; };
+};
+
+class Reduce: public PReduce
 {
 public:
-    Reduce (string theDirInput)
-    :theDir(theDirInput){};
-    int reduce(string keyInput, vector<int> countInput);
-private:
-    string theDir;
-    int exportReduce(string key, int reducedValue);
+	// Constructor that takes in the directory that the export file
+	// should be in and saves it as a class parameter, theDir
+	Reduce(string theDirInput)
+		:theDir(theDirInput) {};
 
+	// Function that takes in the string key and vector of intergers, sums the vector,
+	// and passes the sum and key to the export function to output it to a file
+	int reduce(string keyInput, vector<int> countInput);
+
+private:
+	// Export function called by reduce function that accepts the key and vector sum,
+	// it creates a file in the theDir directory if it does not exist and adds the key and interger to the file
+	int exportReduce(string key, int reducedValue);
+
+	// Variable to hold the directory that is used in the exportReduce function
+	string theDir;
 
 };
+
+//factory function to pass a pointer to a reduce class
+extern "C" REDUCELIBRARY_API PReduce * createReduce(string theDirInput);
